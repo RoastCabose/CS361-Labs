9c42b8bf3ffac320bee64b72bf0242e409a0eacf
RoastCabose <31293308+RoastCabose@users.noreply.github.com>
2017-09-13 15:57:45 -0400

    Add files via upload

--- Lab2/test_DList.py
+++ Lab2/test_DList.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-
 #----------------------------------------------------------------------
 # test_DList.py
-# Dave Reed
+# Tom Green, Andrew McEwen
 # 08/20/2013
 #----------------------------------------------------------------------
 
@@ -80,7 +78,7 @@ class DListTest(unittest.TestCase):
         self.assertEqual(a.pop(0), b.pop(0))
         self.check_list(a, b)
 
-    #------------------------------------------------------------------
+    # ------------------------------------------------------------------
 
     def test_remove(self):
 
@@ -102,6 +100,109 @@ class DListTest(unittest.TestCase):
         self.check_list(a, b)
 
         self.assertRaises(ValueError, a.remove, 10)
+
+    # ------------------------------------------------------------------
+
+    def test_setGetDel(self):
+
+        a = DList([0, 1, 2, 3, 4, 5])
+        self.assertEqual(a[0], 0)
+        self.assertEqual(a[3], 3)
+        self.assertEqual(a[-1], 5)
+        self.assertEqual(a[-3], 3)
+        self.assertRaises(IndexError, a.__getitem__, 10)
+        self.assertRaises(IndexError, a.__getitem__, -8)
+
+        a[4] = 8
+        self.assertEqual(a[4], 8)
+        a[-1] = 12
+        self.assertEqual(a[5], 12)
+        self.assertRaises(IndexError, a.__setitem__, 10, 5)
+
+        a = DList([0, 1, 2, 3, 4, 5])
+        del a[4]
+        self.assertEqual(a[4], 5)
+        del a[-1]
+        self.assertEqual(a.size, 4)
+        self.assertRaises(IndexError, a.__delitem__, 10)
+
+    # ------------------------------------------------------------------
+
+    def test_insert(self):
+
+        a = DList([0, 1, 2, 3, 4, 5])
+        a.insert(3, 24)
+        self.assertEqual(a[3], 24)
+
+        a.insert(0, 10)
+        self.assertEqual(a[0], 10)
+
+        a.insert(9, 7)
+        self.assertEqual(a[-1], 7)
+
+        a.insert(20, 8)
+        self.assertEqual(a[-1], 8)
+
+        a.insert(-20, 42)
+        self.assertEqual(a[0], 42)
+
+    # ------------------------------------------------------------------
+
+    def test_minMax(self):
+
+        a = DList([0,1,2,3,4,5])
+        self.assertEqual(max(a), 5)
+        a.append(-4)
+        self.assertEqual(max(a), 5)
+        a.insert(3, 10)
+        self.assertEqual(max(a), 10)
+
+        a = DList([0, 1, 2, 3, 4, 5])
+        self.assertEqual(min(a), 0)
+        a.append(-4)
+        self.assertEqual(min(a), -4)
+        a.insert(3, -6)
+        self.assertEqual(min(a), -6)
+
+        a = DList()
+        self.assertRaises(ValueError, min, a)
+        self.assertRaises(ValueError, max, a)
+
+    # ------------------------------------------------------------------
+
+    def test_extend(self):
+
+        a = DList([1, 2, 3])
+        b = DList([4, 5])
+        c = DList([])
+        a.extend(b)
+        a.extend(c)
+        self.assertEqual(list(a), [1, 2, 3, 4, 5])
+
+    # ------------------------------------------------------------------
+
+    def test_index(self):
+
+        a = DList([1,5,9,6,8,2,5])
+        self.assertEqual(a.index(1), 0)
+        self.assertEqual(a.index(5), 1)
+        self.assertEqual(a.index(5, 3), 6)
+
+        c = DList()
+        self.assertRaises(IndexError, c.index, 5)
+
+    # ------------------------------------------------------------------
+
+    def test_count(self):
+
+        a = DList([1, 2, 2, 2, 3, 4])
+        b = a.count(2)
+        self.assertEqual(b, 3)
+
+        self.assertEqual(a.count(5), 0)
+
+    # ------------------------------------------------------------------
+
 
 #----------------------------------------------------------------------
 
